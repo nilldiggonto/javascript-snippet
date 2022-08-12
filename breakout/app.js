bab2754199e792f69e2c0c76b3abb5161157fafd
nilldiggonto <moinulantu@gmail.com>
2022-08-12 22:06:20 +0600

    break point

--- breakout/app.js
+++ breakout/app.js
@@ -4,8 +4,15 @@ const blockHeight = 20;
 const userStart = [ 230,10]
 const currentPosition = userStart
 const boardWidth = 450;
+const boardHeight = 600;
 var colors = ['red', 'blue', 'green', 'teal', 'rosybrown', 'tan', 'plum', 'saddlebrown'];
+let ballTimerid
+const ballStart = [300,30]
+let ballCurrentPosition= ballStart;
+const ballDiameter = 20
 
+xDirection = 2
+yDirection= 2
 class Block{
     constructor(xAxis,yAxis){
         this.bottomLeft = [xAxis,yAxis]
@@ -66,11 +73,16 @@ function drawUser(){
     user.style.bottom = currentPosition[1] +'px'
 }
 
+function drawBall(){
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
+    
+}
 
 
 //move user
 function moveUser(e){
-    console.log(e.key);
+    // console.log(e.key);
     switch(e.key){
         
         case 'ArrowLeft':
@@ -95,3 +107,82 @@ function moveUser(e){
 
 document.addEventListener('keydown',moveUser)
 
+//Adding Ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+//moving Ball
+function moveBall(){
+    ballCurrentPosition[0] += xDirection;
+    ballCurrentPosition[1] += yDirection; 
+
+    drawBall();
+    checkForBreak();
+};
+
+ballTimerid = setInterval(moveBall,30);
+
+//Collisions
+function checkForBreak(){
+    //break
+    for (let i= 0; i< blocks.length; i++){
+        if(
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] <blocks[i].bottomRight[0]) &&
+            (ballCurrentPosition[1] + ballDiameter) > (blocks[i].bottomLeft[1]) && (ballCurrentPosition[1] < blocks[i].topLeft[1])
+        ){
+            const allBlocks = document.querySelectorAll('.brick');
+            allBlocks[i].classList.remove('brick')
+            blocks.splice(i,1)
+            changeDirection()
+        }
+    };
+
+    if (ballCurrentPosition[0] >= ( 450 - ballDiameter) || ballCurrentPosition[1] >=(450 - ballDiameter) ||
+        ballCurrentPosition[0] <= 0
+    ){
+        changeDirection()
+    }
+
+
+    if ((ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) &&
+        (ballCurrentPosition[1] >currentPosition[1] && ballCurrentPosition[1] <currentPosition[1] + blockHeight)
+        ){
+            
+            changeDirection()
+        }
+    // Game over
+    if (ballCurrentPosition[1] <= -3){
+        console.log('game over')
+        clearInterval(ballTimerid)
+    }
+}
+
+function changeDirection(){
+    if(xDirection === 2 && yDirection === 2 ){
+        yDirection = -2
+        // console.log('one')
+        return
+    }
+
+    if (xDirection === 2 && yDirection === -2){
+        xDirection = -2
+        // console.log('one2')
+
+        return
+    }
+
+    if (xDirection === -2 && yDirection === -2){
+            yDirection = 2
+        // console.log('one3')
+
+            return
+    }
+    if (xDirection === -2 && yDirection ===2 ){
+        xDirection = 2
+        // console.log('one4')
+
+        return
+    }
+}
